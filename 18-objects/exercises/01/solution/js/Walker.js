6c2d33bbd4fbb69a64b9b1b2018f696138aa6f89
Joe <reszketo.robert@gmail.com>
2022-09-17 21:44:06 +0200

    solution

--- 18-objects/exercises/01/solution/js/Walker.js
+++ 18-objects/exercises/01/solution/js/Walker.js
@@ -1,1 +1,37 @@
+function Walker(name = 'John Doe', state = 'dead') {
+  this.name = name;
+  this.state = state;
+
+  Object.defineProperties(this, {
+    firstName: {
+      get() {
+        return this.name.split(' ')[0];
+      },
+      set(value) {
+        this.name = `${value} ${this.name.split(' ')[1]}`;
+      },
+    },
+    lastName: {
+      get() {
+        return this.name.split(' ')[1];
+      },
+      set(value) {
+        this.name = `${this.name.split(' ')[0]} ${value}`;
+      },
+    },
+  });
+}
+
+Walker.prototype = {
+  walk(speedInMph = 10) {
+    return `${this.name} the ${this.state} walks with ${speedInMph} mph!`;
+  },
+  eat(foodName = 'meat') {
+    return `${this.name} the ${this.state} eats ${foodName}!`;
+  },
+  speak(sound = 'grrrrrr') {
+    return `${this.name} the ${this.state} says ${sound}!`;
+  },
+};
+
 export default Walker;
